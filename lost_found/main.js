5da67453177f14a5774495ac730525e6e7699d55
jackhanss <johanssen@bancroftschool.org>
2018-05-10 09:14:06 -0400

    new text box, data base,

--- lost_found/main.js
+++ lost_found/main.js
@@ -3,7 +3,26 @@
 render();
 
 function render(){
-    console.log('the function ran')
 
+$.ajax({
+
+    url: 'https://bancroftlaf.herokuapp.com/api/getItems',
+    success: function(data){
+        console.log(data)
+        $('#lostItems').html('');
+        for (var i = 0; i <data.length; i++) {
+            $('#lostItems').append(`
+            <div class = "item">
+            <h1>${data[i].item}</h1>
+            <p>${data[i].description}</p>
+            </div>
+            `);
+
+        }
+    }
+
+
+
+})
 
 };
