46c624e9c9bcab1db6173dd024f6f77cf19100ea
jackhanss <johanssen@bancroftschool.org>
2018-05-15 09:18:29 -0400

    everything happy

--- lost_found/main.js
+++ lost_found/main.js
@@ -1,4 +1,4 @@
-
+$('document').ready(function(){
 
 render();
 
@@ -15,14 +15,31 @@ $.ajax({
             <div class = "item">
             <h1>${data[i].item}</h1>
             <p>${data[i].description}</p>
+            <button id = "${data[i]._id}" class = "deleteButton">Delete</button>
             </div>
             `);
 
         }
     }
-
-
-
 })
+}
 
-};
+$('#button').click(function(){
+
+
+    console.log('asdfsadf')
+
+    $.ajax({
+        url: 'https://bancroftlaf.herokuapp.com/api/additem',
+        method: 'POST',
+        data: {
+            itemName: $('#where') .val(),
+            itemDescription: $('#describe').val()
+         },
+         success: function(){
+             render()
+         }
+    })
+    
+});
+})
